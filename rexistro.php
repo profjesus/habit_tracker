93bf5364b07b028d7ca5de558fd4af276053936a
Jesús Martínez <profejesusmartinez@gmail.com>
2019-01-24 10:55:35 +0100

    Navbar added

--- rexistro.php
+++ rexistro.php
@@ -11,6 +11,25 @@
 		$lectura = "SELECT * FROM Habitos;";
 		$habitos = mysqli_query($conn, $lectura);
 	?>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">Habit Tracker</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="habitos.php">Hábitos</a>
+		  </li>
+		  <li class="nav-item active">
+		    <a class="nav-link" href="#">Rexistro</a>
+		  </li>
+		</ul>
+	  </div>
+	</nav>
 	<table class="table">
 		<tr>
 			<td></td>
